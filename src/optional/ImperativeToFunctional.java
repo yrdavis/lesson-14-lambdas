be0a3193e32fbbd7ccb59e24968539a9ffa8e2a3
yrdavis <yrd1@hotmail.com>
2020-05-11 18:02:42 -0700

    Solutions to streams exercises

--- src/optional/ImperativeToFunctional.java
+++ src/optional/ImperativeToFunctional.java
@@ -2,6 +2,7 @@ package optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ImperativeToFunctional {
 
@@ -9,27 +10,29 @@ public class ImperativeToFunctional {
 
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		int result = 0;
-		for (int i = 0; i < numbers.size(); i++) {
-			Integer num = numbers.get(i);
-			if (num > 5 && num % 2 == 0 && num < 9 && num * 2 > 15) {
-				result = num;
-				break;
-			}
-		}
-
+		// for (int i = 0; i < numbers.size(); i++) {
+		// Integer num = numbers.get(i);
+		// if (num > 5 && num % 2 == 0 && num < 9 && num * 2 > 15) {
+		// result = num;
+		// break;
+		// }
+		// }
+//		System.out.println(result);
 		/* 1. Predict the result without running the code. */
-		
+// Guess the result = 8
 
 		/* 2. Write a functional alternative using the filter method. */
+// System.out.print();
+		System.out.printf("The results:  %s%n",
+				numbers.stream().filter(x -> x > 5 && x % 2 == 0 && x < 9 && x * 2 > 15).collect(Collectors.toList()));
 
-		
 		/*
 		 * 3. This example was used in conjunction with a survey to measure the
-		 * difference in time it takes to understand imperative versus
-		 * functional code. 
+		 * difference in time it takes to understand imperative versus functional code.
 		 * 
 		 * Read more about it here:
-		 * http://blog.agiledeveloper.com/2015/08/prefer-functional-style-over-imperative.html
+		 * http://blog.agiledeveloper.com/2015/08/prefer-functional-style-over-
+		 * imperative.html
 		 */
 
 	}
--- src/optional/StreamSorter.java
+++ src/optional/StreamSorter.java
@@ -1,22 +1,22 @@
 package optional;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.Stream;
+import java.security.SecureRandom;
 
 public class StreamSorter {
 	public static void main(String[] args) {
-		Integer[] nums = new Integer[50];
-		for(int i = 0; i < nums.length; i++) {
-			Random rGen = new Random();
-			nums[i] = rGen.nextInt();
-		}
-		
-		//1. Convert the Integer array to Stream object.
-		
-		//2. Use the sorted method of the stream to sort the numbers.
-		
-		//3. Use the forEach method with a lambda to print all the elements of the sorted Stream.
-		//   They should be in ascending order.
+//		Integer[] nums = new Integer[50];
+//		for (int i = 0; i < nums.length; i++) {
+		SecureRandom rGen = new SecureRandom();
+//			nums[i] = rGen.nextInt();
+
+		// 1. Convert the Integer array to Stream object.
+
+		rGen.ints(50)
+				// 2. Use the sorted method of the stream to sort the numbers.
+				.sorted()
+				// 3. Use the forEach method with a lambda to print all the elements of the
+
+				.forEach(System.out::println);
+		// They should be in ascending order.
 	}
 }
